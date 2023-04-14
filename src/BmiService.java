fbbde47d394234e27b5916bc4f1abb9530fe634a
alekanic <koirallabublik@gmail.com>
2023-04-14 12:10:17 +0300

    Second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public int calculate (double kilograms, double meters) {
-        double bmi_index = kilograms / (meters * meters);
-        return (int) bmi_index;
+        double bmiIndex = kilograms / (meters * meters);
+        return (int) bmiIndex;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
         double kilograms = 98;
         double meters = 1.87;
         BmiService service = new BmiService();
-        int my_BmiService = service.calculate(kilograms,meters);
-        System.out.println(my_BmiService);
+        int myBmiService = service.calculate(kilograms,meters);
+        System.out.println(myBmiService);
 
     }
 }
